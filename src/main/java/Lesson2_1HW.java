9dadabf758ef4b598c8b06e994a20cedc1035d07
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-11-12 13:24:02 +0300

    buy git

--- src/main/java/Lesson2_1HW.java
+++ src/main/java/Lesson2_1HW.java
@@ -2,6 +2,6 @@ public class Lesson2_1HW {
 
     public static void main(String[] args){
         System.out.println("Hello word!2");
-        System.out.println("hello git");
+        System.out.println("");
     }
 }
